a46041f157cb53f62709a7c6de5f9be911d5beda
Karl Grossner <karl.geog@gmail.com>
2018-10-17 13:16:05 -0600

    dashboard basics

--- contribute/views.py
+++ contribute/views.py
@@ -1,9 +1,11 @@
 # contribute.views
-
 from django.shortcuts import render
+from main.models import Dataset
 
 def home(request):
     return render(request, 'contribute/home.html')
 
 def dashboard(request):
-    return render(request, 'contribute/dashboard.html')
+    dataset_list = Dataset.objects.filter(user=request.user.id)
+    print('dataset_list',dataset_list)
+    return render(request, 'contribute/dashboard.html', {'datasets':dataset_list})
