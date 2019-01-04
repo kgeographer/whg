6fd39fbecfc7f10f8f25bb44bd8c22180ab3c0a9
Karl Grossner <karl.geog@gmail.com>
2019-01-04 09:51:25 -0700

    working, stalled at saving edited hulls

--- datasets/admin.py
+++ datasets/admin.py
@@ -5,5 +5,4 @@ class DatasetAdmin(admin.ModelAdmin):
     list_display = ('id', 'label', 'name', 'format', 'datatype')
 admin.site.register(Dataset,DatasetAdmin)
 
-# admin.site.register(Link)
 admin.site.register(Hit)
