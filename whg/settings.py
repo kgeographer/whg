1aef9421e918d76ffbc95a3ce6cb3fd0de5adf36
Karl Grossner <karl.geog@gmail.com>
2019-01-19 18:33:12 -0700

    places app basics; pre-mergeback

--- whg/settings.py
+++ whg/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
         ],
         'APP_DIRS': True,
         'OPTIONS': {
+            'debug': True,
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
